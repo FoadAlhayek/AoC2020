afb5b1395266b1dbaf71591d35bbf692ee3c7abd
FoadAlhayek <FoadAlhayek@users.noreply.github.com>
2020-12-08 02:02:27 +0100

    My day 7 solution

--- Day 7/aoc7.m
+++ Day 7/aoc7.m
@@ -9,13 +9,12 @@ fid = fopen('aoc7_input.txt', 'r');
 
 % Initialize
 colorInfo = struct([]);
-searchQueue = struct('match', []);
-currentSearch = 'shiny gold';
+searchedBags = struct('match', []);
+currentSearch = 'shiny_gold';
 i = 1;
 run = 1;
 
 % Search through all unique bags
-tic
 while ~isempty(currentSearch)
   readline = fgetl(fid);
   splitIndex = regexp(readline, 'contain');
@@ -24,18 +23,20 @@ while ~isempty(currentSearch)
   currentColor = regexprep(readline(1:(splitIndex - 7)), '\s', '_'); 
   otherColors = readline((splitIndex + 8):end);  % 8 char for contain & ws
   
-  otherColorInfo = regexp(otherColors,'\w*.\w*.(?= bag)','match');
+  otherColorInfo = regexprep(...
+    regexp(otherColors,'\w*.\w*.(?= bag)','match'), '\s', '_');
   
-  % This if-statement is for task 2
+  % This if-statement is for task 2 - save what each bag contains
   if run == 1
     amountInfo = regexp(readline,'\d+','match');
     currentInfo = struct('color', otherColorInfo, 'amount', amountInfo);
     colorInfo(1).(currentColor) = currentInfo;
   end
-
+  
+  % Store all unique colors in queue to be searched if matched
   if any(strcmp(otherColorInfo, currentSearch))
-    if ~any(strcmp({searchQueue.match}, currentColor))  % Unique check
-      searchQueue(i).match = currentColor;
+    if ~any(strcmp({searchedBags.match}, currentColor))
+      searchedBags(i).match = currentColor;
       i = i + 1;
     end
   end
@@ -43,60 +44,96 @@ while ~isempty(currentSearch)
   % Reset and re-search
   if feof(fid)
     try 
-      currentSearch = regexprep(searchQueue(run).match, '_', ' ');
+      currentSearch = searchedBags(run).match;
       run = run + 1;
       frewind(fid);
     catch
-      break;
+      break;  % All bags have been searched
     end
   end
   
 end
-toc
-
-nUniqueBags = size(searchQueue, 2);
-
-% Task 2
-% Similar as task 1 but we search from the other end instead
-frewind(fid);
-searchQueue = struct('match', []);
-currentSearch = 'shiny gold';
-i = 1;
-run = 1;
-
-tic
-while ~isempty(currentSearch)
-  readline = fgetl(fid);
-  splitIndex = regexp(readline, 'contain');
-  
-  % Rewrite so struct can handle it
-  currentColor = regexprep(readline(1:(splitIndex - 7)), '\s', '_'); 
-  otherColors = readline((splitIndex + 8):end);  % 8 char for contain & ws
-  
-  otherColorInfo = regexp(otherColors,'\w*.\w*.(?= bag)','match');
-  
-  if any(strcmp(currentColor, currentSearch))
-    if ~any(strcmp({searchQueue.match}, currentColor))  % Unique check
-      searchQueue(i).match = currentColor;
-      i = i + 1;
-    end
-  end
-  
-  % Reset and re-search
-  if feof(fid)
-    try 
-      currentSearch = regexprep(searchQueue(run).match, '_', ' ');
-      run = run + 1;
-      frewind(fid);
-    catch
-      break;
-    end
-  end
-  
-  amountInfo = regexp(readline,'\d+','match');
-end
-toc
 
 fclose(fid);
-% 261
-fprintf('Task 1: %d\n', nUniqueBags)
+
+nUniqueBags = size(searchedBags, 2);
+
+% Task 2
+currentSearch = 'shiny_gold';
+
+searchQueue = struct('queue', []);
+colorHistory = struct([]);
+
+queueIndex = 1;
+sumOfBags = 0;
+previousAmount = 1;
+
+% Initialize
+currentInfo = struct('color', currentSearch, 'amount', previousAmount);
+colorHistory(1).(currentSearch) = currentInfo;
+searchQueue(1).queue = currentSearch;
+
+% NOTE: What a bag contains is NOT unique!
+while ~isempty(currentSearch)
+  % Info of what the current bag contains
+  bagColors = {colorInfo.(regexprep(currentSearch, '\d', '')).color};
+  bagAmount = {colorInfo.(regexprep(currentSearch, '\d', '')).amount};
+  
+  % Check how many we have of the current bag
+  previousAmount = colorHistory.(currentSearch).amount;
+  
+  % Get the new amount and sum up
+  newAmount = previousAmount*str2double(bagAmount);
+  sumOfBags = sumOfBags + sum(newAmount);
+  nColors = size(bagColors, 2);
+  
+  % Store the content so it can be be used later when revisited
+  for i = 1:nColors
+    bagColor = bagColors{i};
+    checkDuplicate = contains(fieldnames(colorHistory), bagColor);
+    
+    % If duplicate store it as a new branch
+    if ~any(checkDuplicate)
+      currentInfo = struct('color', bagColor, 'amount', newAmount(i));
+      colorHistory.(bagColor) = currentInfo;
+    else
+      nUnique = sum(checkDuplicate);
+      bagColor = append(bagColor, num2str(nUnique));
+      currentInfo = struct('color', bagColor, 'amount', newAmount(i));
+      colorHistory.(bagColor) = currentInfo;
+    end
+    
+  end
+  
+  nQueue = size(searchQueue, 2) + 1;
+  i = 1;
+  
+  % Add bags to the search queue
+  for j = nQueue:(nColors + (nQueue - 1))
+    bagColor = bagColors{i};
+    checkDuplicate = contains({searchQueue.queue}, bagColor);
+    
+    % If duplicate store it as a new search term
+    if ~any(checkDuplicate)
+      searchQueue(j).queue = bagColor;
+    else
+      nUnique = sum(checkDuplicate);
+      searchQueue(j).queue = append(bagColor, num2str(nUnique));
+    end
+    
+    i = i + 1;
+  end
+  
+  % Pick new bag and go through search queue until the end
+  try
+    queueIndex = queueIndex + 1;
+    currentSearch = searchQueue(queueIndex).queue;
+  catch
+    break;  % All paths of bags have been searched
+  end
+  
+end
+
+fprintf(['Task 1: %d bag colors can at least contain one shiny gold bag\n',...
+  'Task 2: %d individual bags are required inside a single shiny gold bag\n'], ...
+  nUniqueBags, sumOfBags)
